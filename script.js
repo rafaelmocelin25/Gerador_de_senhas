52553a4d26beec966e7538115363c10a511e6203
rafaelmocelin25 <141454436+rafaelmocelin25@users.noreply.github.com>
2025-08-27 09:30:53 -0300

    sads

--- script.js
+++ script.js
@@ -5,9 +5,16 @@ numeroSenha.textcontent = tamanhoSenha;
 const botoes = document.querySelectorAll('.paramero.senha__botao');
 
 botoes[0].onclick = diminuiTamanho;
+botoes[01.onclick = aumentaTamanho;
+
+function aumentaTamanho(){
+    tamanhoSenha = tamanhoSenha + 1;
+    numero.textcontent = tamanhoSenha;
+}
 
 function diminuiTamanho(){
-    tamanhoSenha = tamanhoSenha
+    tamanhoSenha = tamanhoSenha - 1;
+    numero.textcontent = tamanhoSenha;
 }
 
 console.log(botoes);
