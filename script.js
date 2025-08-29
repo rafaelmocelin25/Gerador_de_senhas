0d202a4e94c4e570d951227502d7abf2fe9c83db
rafaelmocelin25 <141454436+rafaelmocelin25@users.noreply.github.com>
2025-08-29 11:05:49 -0300

    h

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const numeroSenha = document.querySelector('.parametro-senha__texto')
 let tamanhoSenha = 12;
 numeroSenha.textcontent = tamanhoSenha;
 
-const botoes = document.querySelectorAll('.paramero.senha__botao');
+const botoes = document.querySelectorAll('.parametro-senha__botao');
 
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
